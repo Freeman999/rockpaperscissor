d7f21bd4cdfc7d8004800329b9482ae296b75b12
Freeman999 <65241975+Freeman999@users.noreply.github.com>
2021-09-29 21:28:42 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        music.playMelody("G B A G C5 B A B ", 500)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     music.playTone(262, music.beat(BeatFraction.Whole))
     basic.pause(1000)
@@ -31,6 +36,12 @@ input.onButtonPressed(Button.B, function () {
             . # # # .
             . # # # .
             `)
+    }
+    basic.pause(5000)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 1; index++) {
+        music.playMelody("F G E F D E C D ", 100)
     }
 })
 let hand = 0
