5988ac59265c21405d08d23430591a68d6159863
Freeman999 <65241975+Freeman999@users.noreply.github.com>
2021-09-29 21:14:02 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,45 @@
-let text_list: string[] = []
-basic.forever(function () {
-    text_list = ["a", "b", "c"]
-    music.playMelody("- - - - - - - - ", 0 + 0)
-    control.waitMicros(4)
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(1000)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(1000)
+    music.playMelody("G E F D C5 - - - ", 170)
+    hand = randint(0, 2)
+    if (hand == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (hand == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
 })
+let hand = 0
+basic.showLeds(`
+    # . # . #
+    # . # . #
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.pause(1000)
+basic.showIcon(IconNames.ArrowEast)
