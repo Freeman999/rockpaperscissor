41a46b8f4da16a0557881e699d396a20d194e0ea
Freeman999 <65241975+Freeman999@users.noreply.github.com>
2021-10-01 11:07:09 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+# Punkte mit Variable "score" sammeln und bei Schütteln score anzeigen
+
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HAPPY)
+    for index in range(2):
+        music.play_melody("G B A G C5 B A B ", 500)
+    # soll anzeigen, wieviele Punkte und +1
+    # 
+    basic.show_string("hi!")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global score, hand
+    basic.clear_screen()
+    score = 0
+    music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    basic.pause(1000)
+    music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    basic.pause(1000)
+    music.play_melody("G E F D C5 - - - ", 170)
+    hand = 0
+    hand = randint(0, 2)
+    if hand == 0:
+        basic.show_leds("""
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            """)
+    if hand == 1:
+        basic.show_icon(IconNames.SCISSORS)
+    if hand == 2:
+        basic.show_leds("""
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            """)
+    basic.pause(5000)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_icon(IconNames.SAD)
+    for index2 in range(1):
+        music.play_melody("F G E F D E C D ", 100)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+hand = 0
+score = 0
+basic.show_leds("""
+    # . # . #
+    # . # . #
+    # # # . #
+    # . # . #
+    # . # . #
+    """)
+basic.pause(1000)
+basic.show_leds("""
+    . . . . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . . . .
+    """)
+
+    
--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
-input.onButtonPressed(Button.A, function () {
+//  Punkte mit Variable "score" sammeln und bei Schütteln score anzeigen
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showIcon(IconNames.Happy)
     for (let index = 0; index < 2; index++) {
         music.playMelody("G B A G C5 B A B ", 500)
     }
+    //  soll anzeigen, wieviele Punkte und +1
+    //  
+    basic.showString("hi!")
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     basic.clearScreen()
+    score = 0
     music.playTone(262, music.beat(BeatFraction.Whole))
     basic.pause(1000)
     music.playTone(262, music.beat(BeatFraction.Whole))
@@ -22,9 +28,11 @@ input.onButtonPressed(Button.AB, function () {
             . . # . .
             `)
     }
+    
     if (hand == 1) {
         basic.showIcon(IconNames.Scissors)
     }
+    
     if (hand == 2) {
         basic.showLeds(`
             . # # # .
@@ -34,15 +42,17 @@ input.onButtonPressed(Button.AB, function () {
             . # # # .
             `)
     }
+    
     basic.pause(5000)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.showIcon(IconNames.Sad)
-    for (let index = 0; index < 1; index++) {
+    for (let index2 = 0; index2 < 1; index2++) {
         music.playMelody("F G E F D E C D ", 100)
     }
 })
 let hand = 0
+let score = 0
 basic.showLeds(`
     # . # . #
     # . # . #
